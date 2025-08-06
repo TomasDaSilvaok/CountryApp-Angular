15faddfd6e27b56bd14f4cd7d9d65aa473f177b3
TomasDanielDasilva <tomas.dasilva@suricata.la>
2025-08-06 16:07:17 -0300

    added countries components and routing module

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,8 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CountriesRoutingModule } from './countries-routing.module';
+import { NgModule } from '@angular/core';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [
+    ByCapitalPageComponent,
+    ByRegionPageComponent,
+    ByCountryPageComponent,
+    CountryPageComponent
+  ],
+  imports: [CommonModule, CountriesRoutingModule],
 })
 export class CountriesModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about/about.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 @NgModule({
-  declarations: [HomePageComponent, AboutPageComponent],
-  imports: [CommonModule],
-  exports: [AboutPageComponent, HomePageComponent],
+  declarations: [HomePageComponent, AboutPageComponent, SidebarComponent, ContactPageComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [AboutPageComponent, HomePageComponent, SidebarComponent],
 })
 export class SharedModule {}
