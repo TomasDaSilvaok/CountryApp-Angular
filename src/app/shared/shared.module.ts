7e7828038cd51a968f9d35a9c2535d73116680e0
TomasDanielDasilva <tomas.dasilva@suricata.la>
2025-08-06 15:26:54 -0300

    added basic routing

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about/about.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomePageComponent, AboutPageComponent],
+  imports: [CommonModule],
+  exports: [AboutPageComponent, HomePageComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
